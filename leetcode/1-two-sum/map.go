8c6612facd0927d7e51d6813255e7173cc892581
tsungyu927 <tsungyu927@gmail.com>
2024-11-24 14:54:37 +0800

    leetcode: 1

---
+++ leetcode/1-two-sum/map.go
@@ -0,0 +1,23 @@
+/*
+	Problem: Two Sum
+	Link: https://leetcode.com/problems/two-sum/description/
+*/
+
+package __two_sum
+
+func twoSum(nums []int, target int) []int {
+	numMap := make(map[int]int)
+
+	for i, num := range nums {
+		expect := target - num
+
+		if idx, found := numMap[expect]; found {
+			return []int{idx, i}
+		}
+
+		// Store current num & i to numMap
+		numMap[num] = i
+	}
+
+	return nil
+}
